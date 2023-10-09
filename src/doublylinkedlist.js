d71dac7525e8056fd479047e5538de386ce1fbe3
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-10-09 16:09:16 -0700

    Added doublylinkedlist class Added treenode class

---
+++ src/doublylinkedlist.js
@@ -0,0 +1,35 @@
+import TreeNode from './treenode.js';
+
+export default class DoublyLinkedList {
+    constructor () {
+        this.head = null
+        this.end = null
+    }
+
+    insertEnd(value) {
+        let node = new TreeNode(value)
+        if (this.head === null) {
+            this.head = node
+            this.end = node
+            return
+        }
+        node.previous = this.end
+        this.end.next = node
+        this.end = this.end.next
+    }
+
+    deletenode() {
+        if (this.head === null) {
+            throw new Error('Linked List in empty')
+        }
+        if (this.head.next === null) {
+            this.head = null
+            this.end = null
+            return
+        }
+        else {
+            this.end = this.end.previous
+            this.end.next = null
+        }
+    }
+}
---
+++ src/treenode.js
@@ -0,0 +1,7 @@
+export default class TreeNode {
+    constructor(value) {
+        this.value = value
+        this.previous = null
+        this.next = null 
+    }
+}
