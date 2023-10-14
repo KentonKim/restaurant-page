a1b86b920f93e2146e29a9d697a7777f8e66045b
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-10-14 01:01:59 -0700

    Added color palette for styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,28 @@ module.exports = {
     extend: {
       transitionProperty: {
         width: "width",
-        
       },
       width: {
         100: "25rem",
         200: "50rem",
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'moon-raker': {
+        50: '#f7f6fc',
+        100: '#f0eef9',
+        200: '#e2e0f4',
+        300: '#c9c2ea',
+        400: '#b2a6df',
+        500: '#9781d1',
+        600: '#8465c2',
+        700: '#7353ae',
+        800: '#604592',
+        900: '#503a78',
+        950: '#332550',
       },
     },
   },
